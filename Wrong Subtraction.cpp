a240d388341b359ec2fd7ea9339293833b180b2c
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-03-27 21:27:54 +0530

    Add files via upload

---
+++ Wrong Subtraction.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main () 
+{
+	int n , k , r ;
+	cin >> n >> k ;
+		for ( int i = 1 ; i <= k ; i++ )
+		{
+			r = n % 10 ;
+			if ( r == 0 )
+			{
+				n = n / 10 ;
+				
+			}
+			else 
+			{
+				n = n - 1 ;
+			}
+		}
+
+	cout << n << endl ;
+	return 0;
+}

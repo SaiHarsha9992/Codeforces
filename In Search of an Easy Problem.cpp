8e82c835939cdfd275b2ac268909d05f9710cb91
SaiHarsha9992 <119718764+SaiHarsha9992@users.noreply.github.com>
2023-04-10 21:18:44 +0530

    Add files via upload

---
+++ In Search of an Easy Problem.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main ( )
+{
+	int n ;
+	cin >> n ;
+	int a [ n ] ;
+	for ( int i = 0 ; i < n ; i++ )
+	{
+		cin >> a [ i ] ;
+	}
+	int c = 0 ;
+	for ( int i = 0 ; i < n ; i++ )
+	{
+		if ( a [ i ] > 0 )
+		{
+			c = 1;
+		}
+	}
+	if ( c == 1 )
+	{
+		cout << "HARD" << endl;
+	}
+	else
+	{
+		cout << "EASY" <<endl;
+	}
+    return 0;
+}

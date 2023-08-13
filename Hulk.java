00393ed4ab63d77b17317c31d799031196937e17
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-08-13 20:45:52 +0530

    Add files via upload

---
+++ Hulk.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Hulk
+{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for(int i = 1 ; i < a ; i++){
+			if(i%2!=0){
+				System.out.print("I hate that ");
+			}
+			if(i%2==0){
+				System.out.print("I love that ");
+			}
+		}
+		if(a%2!=0){
+			System.out.print("I hate it ");
+		}
+		if(a%2==0){
+			System.out.print("I love it ");
+		}
+	}
+}
